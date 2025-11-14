3d3a2e6f0231edb38f93de8bb2b325d311fe1da2
The Tan <truongthetan@v-takeuchi.vn>
2025-11-14 16:47:39 +0700

    add doc buoi 3, 4

---
+++ docs/20251116_buoi4/examples/.ipynb_checkpoints/example_matplotlib-checkpoint.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# df = pd.DataFrame({
+#     "weight": [50, 60, 55, 70, 65],
+#     "mpg": [30, 28, 32, 25, 27]
+# })
+
+# sns.scatterplot(data=df, x="weight", y="mpg")
+# plt.title("Weight vs MPG")
+# plt.show()
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print("Trung bình:", np.mean(arr))
+print("Tổng:", np.sum(arr))
